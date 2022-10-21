ef6b5c2bb5369704d57be12742c8d7dc27a0bde9
eEsko <Luxqy1@gmail.com>
2022-10-21 23:34:05 +0200

    laracast11

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="/app.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My first post</a></h1>
+        <p>Sweet tart liquorice sugar plum chocolate cake lemon drops chocolate cake tiramisu gummies. Shortbread powder tart brownie sweet roll. Cotton candy sugar plum jelly beans dessert halvah tart. Jelly beans dessert chocolate bar pudding brownie pudding tootsie roll tootsie roll shortbread.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My second post</a></h1>
+        <p>Sweet tart liquorice sugar plum chocolate cake lemon drops chocolate cake tiramisu gummies. Shortbread powder tart brownie sweet roll. Cotton candy sugar plum jelly beans dessert halvah tart. Jelly beans dessert chocolate bar pudding brownie pudding tootsie roll tootsie roll shortbread.</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My third post</a></h1>
+        <p>Sweet tart liquorice sugar plum chocolate cake lemon drops chocolate cake tiramisu gummies. Shortbread powder tart brownie sweet roll. Cotton candy sugar plum jelly beans dessert halvah tart. Jelly beans dessert chocolate bar pudding brownie pudding tootsie roll tootsie roll shortbread.</p>
+    </article>
+</body>
+</html>
